8a3f2a5f7ac07c1b06fe3667b682df047d00abb9
13 <13@users.noreply.github.com>
2026-04-14 09:21:44 +0200

    added dynamically versioning #2

--- frontend/src/constants/appInfo.ts
+++ frontend/src/constants/appInfo.ts
@@ -1,5 +1,6 @@
+import pkg from '../../package.json';
 export const APP_NAME = 'caero'
 export const APP_TAGLINE = 'price tracker'
 export const APP_DESCRIPTION =
   'Track product prices, detect changes quickly,and manage alerts.'
-export const APP_VERSION = '0.2.2'
+export const APP_VERSION = pkg.version
