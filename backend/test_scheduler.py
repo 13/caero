1254a8cd61fd214a38bad1fb5e6b1fbb9b67e5c2
13 <13@users.noreply.github.com>
2026-04-14 11:16:54 +0200

    clean

--- backend/test_scheduler.py
+++
@@ -1,2 +0,0 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-# dummy print to see the path if I do it inside docker
--- backend/test_tomllib.py
+++
@@ -1,6 +0,0 @@
-import tomllib
-from pathlib import Path
-def get_version():
-    with open(Path(__file__).parent / "pyproject.toml", "rb") as f:
-        return tomllib.load(f)["project"]["version"]
-print(get_version())
